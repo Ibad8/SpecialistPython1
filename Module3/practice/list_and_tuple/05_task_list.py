772893989f1b5b11e905f01f15809da367947d64
Ibad8 <88583648+Ibad8@users.noreply.github.com>
2021-08-14 16:27:05 +0300

    Update 05_task_list.py

--- Module3/practice/list_and_tuple/05_task_list.py
+++ Module3/practice/list_and_tuple/05_task_list.py
@@ -6,7 +6,22 @@
 fruits = ["яблоко", "банан", "киви", "арбуз"]
 
 # TODO: your code here
+max_len = None
+for fruit in fruits:
+    letters = len(fruit)
+    if max_len is None or letters > max_len :
+        max_len = letters
+print(max_len)
 
+n = 1
+
+for fruit in fruits:
+    if len(fruit) == max_len:
+        print(n,'.',fruit)
+        n+=1
+    else:
+        print(n,'.',' ' * (max_len - len(fruit)-1), fruit)
+        n+=1
 # Пример вывода:
 # 1. яблоко
 # 2.  банан
